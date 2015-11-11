b3f90e23e91a12df6e88e221317bc94361ce54b6
Taku AMANO <taku@taaas.jp>
2015-11-11 11:04:55 +0900

    Initial commit.

---
+++ public/constant.rb
@@ -0,0 +1,24 @@
+JUMP_LAYERS=25
+
+MESH_UNIT=30
+
+BACKGROUND_HEIGHT=384
+GROUND_HEIGHT=112
+SCREEN_WIDTH=288
+SCREEN_HEIGHT=BACKGROUND_HEIGHT + GROUND_HEIGHT
+
+TUBE_WIDTH=52
+TUBE_HEIGHT=320
+
+TUBES = [
+  {
+    top:  200,
+    left: 300,
+    space: 70,
+  },
+  {
+    top:  150,
+    left: 500,
+    space: 70,
+  }
+]
